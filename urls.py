3381aa0cd67be81f261c7ac0630e32f8472c92c4
NSaiCharith <77220421+NSaiCharith@users.noreply.github.com>
2021-01-10 10:22:04 +0530

    Delete urls.py

--- urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path, include
-from . import views
-urlpatterns=[
-            path('Home', views.Home, name="Home"),
-            path('Info', views.Info, name="Info"),
-            path('Transfer', views.Transfer, name="Transfer"),
-            path('Transfer1', views.Transfer1, name="Transfer1"),   
-            path('TranInfo', views.TranInfo, name="TranInfo")    
-        ]
